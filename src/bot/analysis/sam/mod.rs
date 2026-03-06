e526a4ea713f132ce8dd7b5e82b6928d13c5900b
Victor <theonlypwner@users.noreply.github.com>
2026-03-05 17:12:29 -0700

    refactor: clean up LinkCutTree

--- src/bot/analysis/sam/mod.rs
+++ src/bot/analysis/sam/mod.rs
@@ -11,8 +11,6 @@ struct Node<T> {
     rev: bool,
 
     has_assign: bool,
-    assign_val: T,
-
     val: T,
 }
 
@@ -25,15 +23,12 @@ impl<T: Copy + Default> Node<T> {
             rev: false,
 
             has_assign: false,
-            assign_val: T::default(),
-
             val,
         }
     }
 
     fn apply_assign(&mut self, val: T) {
         self.val = val;
-        self.assign_val = val;
         self.has_assign = true;
     }
 }
@@ -51,10 +46,21 @@ impl<T: Copy + Default> LinkCutTree<T> {
         self.nodes.push(Node::new(initial_value));
     }
 
-    // Change parent of u to newParent
-    pub fn change_parent(&mut self, u: usize, new_parent: usize) {
-        self.cut_parent(u); // detach from old parent if any
-        self.link_parent(u, new_parent); // attach under newParent
+    // Link x as a child of p (assumes no cycle is created)
+    pub fn link_parent(&mut self, x: usize, p: usize) {
+        self.make_root(x);
+        self.nodes[x].parent = p as isize;
+    }
+
+    // Cut x from its parent, if any
+    pub fn cut_parent(&mut self, x: usize) {
+        self.access(x);
+        self.splay(x);
+        let left = self.nodes[x].left;
+        if left != -1 {
+            self.nodes[left as usize].parent = -1;
+            self.nodes[x].left = -1;
+        }
     }
 
     // Assign value to u and all its ancestors
@@ -72,16 +78,17 @@ impl<T: Copy + Default> LinkCutTree<T> {
 
     // ---------- Core LCT primitives ----------
 
-    fn is_root(&self, u: usize) -> bool {
-        let x = &self.nodes[u];
-        x.parent == -1
-            || (self.nodes[x.parent as usize].left != u as isize
-                && self.nodes[x.parent as usize].right != u as isize)
+    fn is_root(&self, x: usize) -> bool {
+        let n = &self.nodes[x];
+        n.parent == -1 || {
+            let p = &self.nodes[n.parent as usize];
+            p.left != x as isize && p.right != x as isize
+        }
     }
 
-    fn push_down(&mut self, u: usize) {
-        if self.nodes[u].rev {
-            let node = &mut self.nodes[u];
+    fn push_down(&mut self, x: usize) {
+        if self.nodes[x].rev {
+            let node = &mut self.nodes[x];
             swap(&mut node.left, &mut node.right);
             let left = node.left;
             let right = node.right;
@@ -91,63 +98,63 @@ impl<T: Copy + Default> LinkCutTree<T> {
             if right != -1 {
                 self.nodes[right as usize].rev ^= true
             }
-            self.nodes[u].rev = false;
+            self.nodes[x].rev = false;
         }
 
-        if self.nodes[u].has_assign {
-            let left = self.nodes[u].left;
-            let right = self.nodes[u].right;
-            let val = self.nodes[u].assign_val;
+        if self.nodes[x].has_assign {
+            let left = self.nodes[x].left;
+            let right = self.nodes[x].right;
+            let val = self.nodes[x].val;
             if left != -1 {
                 self.nodes[left as usize].apply_assign(val)
             }
             if right != -1 {
                 self.nodes[right as usize].apply_assign(val)
             }
-            self.nodes[u].has_assign = false;
+            self.nodes[x].has_assign = false;
         }
     }
 
-    fn rotate(&mut self, u: usize) {
-        let p = self.nodes[u].parent;
+    fn rotate(&mut self, x: usize) {
+        let p = self.nodes[x].parent;
         let g = self.nodes[p as usize].parent;
         self.push_down(p as usize);
-        self.push_down(u);
+        self.push_down(x);
 
-        let is_left = u == self.nodes[p as usize].left as usize;
+        let is_left = x == self.nodes[p as usize].left as usize;
         let b = if is_left {
-            self.nodes[u].right
+            self.nodes[x].right
         } else {
-            self.nodes[u].left
+            self.nodes[x].left
         };
 
         if !self.is_root(p as usize) {
             if p == self.nodes[g as usize].left {
-                self.nodes[g as usize].left = u as isize
+                self.nodes[g as usize].left = x as isize
             } else if p == self.nodes[g as usize].right {
-                self.nodes[g as usize].right = u as isize
+                self.nodes[g as usize].right = x as isize
             }
         }
-        self.nodes[u].parent = g;
+        self.nodes[x].parent = g;
 
         if is_left {
-            self.nodes[u].right = p;
+            self.nodes[x].right = p;
             self.nodes[p as usize].left = b;
         } else {
-            self.nodes[u].left = p;
+            self.nodes[x].left = p;
             self.nodes[p as usize].right = b;
         }
 
         if b != -1 {
             self.nodes[b as usize].parent = p;
         }
-        self.nodes[p as usize].parent = u as isize
+        self.nodes[p as usize].parent = x as isize
     }
 
-    fn splay(&mut self, u: usize) {
+    fn splay(&mut self, x: usize) {
         {
             let mut stack = Vec::new();
-            let mut y = u;
+            let mut y = x;
             stack.push(y);
             while !self.is_root(y) {
                 y = self.nodes[y].parent as usize;
@@ -156,52 +163,35 @@ impl<T: Copy + Default> LinkCutTree<T> {
             stack.iter().rev().for_each(|&v| self.push_down(v));
         }
 
-        while !self.is_root(u) {
-            let p = self.nodes[u].parent as usize;
+        while !self.is_root(x) {
+            let p = self.nodes[x].parent as usize;
             let g = self.nodes[p].parent;
             if !self.is_root(p) {
-                let up_left = u == self.nodes[p].left as usize;
+                let up_left = x == self.nodes[p].left as usize;
                 let pg_left = p == self.nodes[g as usize].left as usize;
-                self.rotate(if up_left == pg_left { p } else { u });
+                self.rotate(if up_left == pg_left { p } else { x });
             }
-            self.rotate(u);
+            self.rotate(x);
         }
     }
 
     // Expose path root -> x; x becomes root of its auxiliary tree
-    fn access(&mut self, u: usize) {
+    fn access(&mut self, x: usize) {
         let mut last = -1;
-        let mut y = u as isize;
+        let mut y = x as isize;
         while y != -1 {
             self.splay(y as usize);
             self.nodes[y as usize].right = last;
             last = y;
             y = self.nodes[y as usize].parent;
         }
-        self.splay(u);
+        self.splay(x);
     }
 
     // Make x the root of its represented tree
     fn make_root(&mut self, x: usize) {
         self.access(x);
         self.nodes[x].rev ^= true;
-    }
-
-    // Link x as a child of p (assumes no cycle is created)
-    fn link_parent(&mut self, x: usize, p: usize) {
-        self.make_root(x);
-        self.nodes[x].parent = p as isize;
-    }
-
-    // Cut x from its parent, if any
-    fn cut_parent(&mut self, u: usize) {
-        self.access(u);
-        self.splay(u);
-        let left = self.nodes[u].left;
-        if left != -1 {
-            self.nodes[left as usize].parent = -1;
-            self.nodes[u].left = -1;
-        }
     }
 }
 
@@ -269,7 +259,7 @@ impl SuffixAutomaton {
 
         if p == -1 {
             self.st[cur].link = 0;
-            self.lct.change_parent(cur, 0);
+            self.lct.link_parent(cur, 0);
             self.index_of_next = 0; // 0 when not found
         } else {
             let q = self.st[p as usize].next[c as usize];
@@ -280,16 +270,18 @@ impl SuffixAutomaton {
 
             if self.st[p as usize].len + 1 == self.st[qu].len {
                 self.st[cur].link = q;
-                self.lct.change_parent(cur, qu);
+                self.lct.link_parent(cur, qu);
             } else {
                 let clone = self.st.len();
                 self.st.push(self.st[qu].clone());
                 self.st[clone].len = self.st[p as usize].len + 1;
 
                 self.lct.add_vertex(self.index_of_next);
-                self.lct.change_parent(clone, self.st[qu].link as usize);
-                self.lct.change_parent(qu, clone);
-                self.lct.change_parent(cur, clone);
+                self.lct.cut_parent(clone);
+                self.lct.link_parent(clone, self.st[qu].link as usize);
+                self.lct.cut_parent(qu);
+                self.lct.link_parent(qu, clone);
+                self.lct.link_parent(cur, clone);
 
                 while p != -1 && self.st[p as usize].next[c as usize] == q {
                     self.st[p as usize].next[c as usize] = clone as isize;
